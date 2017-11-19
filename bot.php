8467f4f0614a01b342bf3207277b0f2f67921f59
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2017-11-19 18:14:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,17 @@ $Get_Status = file_get_contents($Url);
 
 // Parse JSON
 $events = json_decode($content, true);
-$event_Status = json_decode($Get_Status, true);
+function Void_loop(){
+	while(true) {
+		$event_Status = json_decode($Get_Status, true);
+		$Old = Check_Status($event_Status['result'],$event_Status['value']);	
+		if (strcmp($Now,$Old)){
+		$Now = $Old ;
+		return true;
+		}
+		return  false;
+	}
+}
 
 // Validate parsed JSON data
 function Check_Status($Result,$Value){
@@ -29,68 +39,69 @@ function Check_Status($Result,$Value){
 	return "เออเรอ";		
 }
 
-if (!is_null($events['events'])) {
-	// Loop through each event	
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			if(strtoupper($text) == "GETSTATUS" || $text == "สถานะ"){				
-				$messages = [
-				'type' => 'text',
-				'text' => 'สถานะ '.Check_Status($event_Status['result'],$event_Status['value'])
-				];	
-			} elseif ($text == strtoupper("ON") || $text == "เปิด" ) {
-				$_Status = 1;
-				$messages = [
-				'type' => 'text',
-				'text' => 'เปิด เรียบร้อย'
+	if (!is_null($events['events']) || Void_loop() == true) {
+		// Loop through each event	
+		foreach ($events['events'] as $event) {
+			// Reply only when message sent is in 'text' format
+			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+				// Get text sent
+				$text = $event['message']['text'];
+				// Get replyToken
+				$replyToken = $event['replyToken'];
+				// Build message to reply back
+				if(strtoupper($text) == "GETSTATUS" || $text == "สถานะ"){				
+					$messages = [
+					'type' => 'text',
+					'text' => 'สถานะ '.Check_Status($event_Status['result'],$event_Status['value'])
+					];	
+				} elseif ($text == strtoupper("ON") || $text == "เปิด" ) {
+					$_Status = 1;
+					$messages = [
+					'type' => 'text',
+					'text' => 'เปิด เรียบร้อย'
+					];
+				} elseif ($text == strtoupper("OFF") || $text == "ปิด" ) {
+					$_Status = 0;		
+					$messages = [
+					'type' => 'text',
+					'text' => 'ปิด เรียบร้อย'
+					];					
+				}else {				
+					$messages = [
+					'type' => 'text',
+					'text' => 'กรุณากรอกใหม่...'
+					];				
+				} 
+
+				if ($_Status > -1) {
+					$Get_Status = file_get_contents($Url_Update.$_Status);	
+					$event_Status = json_decode($Get_Status, true);
+					$messages .= [
+					'type' => 'text',
+					'text' => 'สถานะ '.Check_Status($event_Status['result'],$event_Status['value'])
+					];	
+				}
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
 				];
-			} elseif ($text == strtoupper("OFF") || $text == "ปิด" ) {
-				$_Status = 0;		
-				$messages = [
-				'type' => 'text',
-				'text' => 'ปิด เรียบร้อย'
-				];					
-			}else {				
-				$messages = [
-				'type' => 'text',
-				'text' => 'กรุณากรอกใหม่...'
-				];				
-			} 
-				
-			if ($_Status > -1) {
-			   	$Get_Status = file_get_contents($Url_Update.$_Status);	
-				$event_Status = json_decode($Get_Status, true);
-				$messages = [
-				'type' => 'text',
-				'text' => 'สถานะ '.Check_Status($event_Status['result'],$event_Status['value'])
-				];	
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+				echo $result . "\r\n";
+
 			}
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
-			
 		}
 	}
-}
+
 echo "OK";
 echo "<br>".$Url;
