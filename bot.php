984da6a7c12df0c4e7ba33382d6e22d27ef6783e
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2017-11-19 17:06:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -77,7 +77,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => 'Status = '.$_Status
-				];
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
