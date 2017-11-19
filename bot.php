e4c8571eb17f0e95c92385f8aafb984a6d64c7ac
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2017-11-19 22:05:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'SG3JBmTZEAbFxdexsK8JwxrPOle/+IFg/R3TmQKfVIUuJSrcRJOsffKPXXtrEy0q3HahfPxP4P4lxrFbFFWpC7fgOPtEAtxSoSChY3fTJbK2JQfaTqHOxh+EMffXSCLdBlzmkn5OVTtLk3lyD+7VkQdB04t89/1O/w1cDnyilFU=';
-$ composer require linecorp/line-bot-sdk
+$composer require linecorp/line-bot-sdk
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3d61b46618a33ed31fca7f69eb01ac53']);
 
