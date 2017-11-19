75e8e199c613c6ff127c7e8066c1cfccbb50f7b0
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2017-11-19 20:09:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -41,14 +41,14 @@ $event_Status = json_decode($contents, true);
 							
 					$messages = [
 					'type' => 'text',
-					'text' => Check_Status(file_get_contents($Url)) . $replyToken
+					'text' => 'Test'
 					];
 				// Make a POST Request to Messaging API to reply to sender
 				
 				$url = 'https://api.line.me/v2/bot/message/reply';
 				
 				$data = [
-					'replyToken' => $replyToken,				
+					 "to": => ["1547339762"],				
 					'messages' => [$messages],									
 				];
 				$post = json_encode($data);
