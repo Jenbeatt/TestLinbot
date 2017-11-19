65afcf8f761baea97e306d1c2c71c1d6599b3d18
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2017-11-19 13:15:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,11 +18,17 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($text == "Token"){
+			$messages = [
+			'type' => 'text',
+			'text' => $result
+			];
+			}else{
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
+			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -42,7 +48,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			echo "Test";
+			
 		}
 	}
 }
