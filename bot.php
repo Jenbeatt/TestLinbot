dccf37e930e6055d7d72dbe17ef7ecf841c51b61
Jenbeatt <33797716+Jenbeatt@users.noreply.github.com>
2017-11-19 15:29:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,14 +1,35 @@
 <?php
 $access_token = 'SG3JBmTZEAbFxdexsK8JwxrPOle/+IFg/R3TmQKfVIUuJSrcRJOsffKPXXtrEy0q3HahfPxP4P4lxrFbFFWpC7fgOPtEAtxSoSChY3fTJbK2JQfaTqHOxh+EMffXSCLdBlzmkn5OVTtLk3lyD+7VkQdB04t89/1O/w1cDnyilFU=';
 //$access_token = 'iA00aKCsapdGJ2NY1g1W4XIqjaMCYUbVShtwKRb9psC';
+$Token_anto = 'TRJxeh7OfX0WY9dEY7IBdq62h4nwkpNDJu0I6wEb';
+$key = 'NodeMCU';
 
 // Get POST body content
+
 $content = file_get_contents('php://input');
-$Get_Status = file_get_contents('https://api.anto.io/channel/get/TRJxeh7OfX0WY9dEY7IBdq62h4nwkpNDJu0I6wEb/NodeMCU/Status');
+$Get_Status = file_get_contents('https://api.anto.io/channel/get/'. $Token_anto .'/'.$key.'/Status');
 // Parse JSON
 $events = json_decode($content, true);
 $event_Status = json_decode($Get_Status, true);
 // Validate parsed JSON data
+function Check_Status()
+{
+	if($text == strtoupper("GETSTATUS") || $text == "สถานะ" ){				
+		if ($event_Status['result'] == "true") {
+			if ($event_Status['value'] == "0"){
+				$messages = [
+				'type' => 'text',
+				'text' => 'ปิด'//$event_Status['events']['value'];
+				];
+			}else{
+				$messages = [
+				'type' => 'text',
+				'text' => 'เปิด'//$event_Status['events']['value'];
+				];
+			}
+		}    
+}
+
 if (!is_null($events['events'])) {
 	// Loop through each event	
 
@@ -21,25 +42,40 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			if($text == "GetStatus"){				
-					if ($event_Status['result'] == "true" && $event_Status['value'] == "0") {
-					$messages = [
-					'type' => 'text',
-					'text' => 'ปิด'//$event_Status['events']['value'];
-					];
-					}
-					else{
-					$messages = [
-					'type' => 'text',
-					'text' => 'เปิด'.$event_Status['value'].$event_Status['result']//$event_Status['events']['value'];
-					];
-					}						
+			/*if($text == strtoupper("GETSTATUS") || $text == "สถานะ" ){				
+					if ($event_Status['result'] == "true") {
+						if ($event_Status['value'] == "0"){
+							$messages = [
+							'type' => 'text',
+							'text' => 'ปิด'//$event_Status['events']['value'];
+							];
+						}else
+						{
+							$messages = [
+							'type' => 'text',
+							'text' => 'เปิด'//$event_Status['events']['value'];
+							];
+						}
+					}	*/
+				Check_Status();
 							
-			}else{
-			$messages = [
+			}else if ($text == strtoupper("ON") || $text == "เปิด" ){
+				$_Status = 1
+				$messages = [
 				'type' => 'text',
-				'text' => $text
-			];
+				'text' => 'เปิด เรียบร้อย'
+				];
+			}else if ($text == strtoupper("OFF") || $text == "ปิด" ){
+				$_Status = 0				
+				$messages = [
+				'type'$_Status = 0 => 'text',
+				'text' => 'ปิด เรียบร้อย'
+				];
+			}else{ $_Status = -1 }
+			if(!$_Status == -1){
+				$Get_Status = file_get_contents('https://api.anto.io/channel/set/'. $Token_anto .'/'.$key.'/Status/'.$_Status);
+				$event_Status = json_decode($Get_Status, true);
+				Check_Status();
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
